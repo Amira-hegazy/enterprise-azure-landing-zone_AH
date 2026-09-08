bfb1ecade77194a1f5b8e5c6af3a8934dc0f3106
Amira-hegazy <62332040+Amira-hegazy@users.noreply.github.com>
2026-09-08 17:43:13 +0200

    Add GatewaySubnet definition to hub Terraform example

--- infra/terraform/hub/main.tf
+++ infra/terraform/hub/main.tf
@@ -9,3 +9,9 @@ resource "azurerm_virtual_network" "hub" {
   location            = azurerm_resource_group.hub.location
   address_space       = ["10.20.0.0/20"]
 }
+resource "azurerm_subnet" "gateway" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.20.0.0/27"]
+}
