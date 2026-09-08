e5dbaddac032c367209871a321def19f16e084ab
Amira-hegazy <62332040+Amira-hegazy@users.noreply.github.com>
2026-09-08 17:38:02 +0200

    Add hub virtual network definition in Terraform

--- infra/terraform/hub/main.tf
+++ infra/terraform/hub/main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "hub" {
   name     = "rg-hub-example"
   location = var.location
 }
+
+resource "azurerm_virtual_network" "hub" {
+  name                = "vnet-hub-example"
+  resource_group_name = azurerm_resource_group.hub.name
+  location            = azurerm_resource_group.hub.location
+  address_space       = ["10.20.0.0/20"]
+}
